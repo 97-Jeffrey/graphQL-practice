6d54581072e6cc1d2612b59fcae419b332abc3c7
Yu Hao Shao <yuhaoshao@YudeMacBook-Air.local>
2021-03-14 21:04:47 -0700

    the authors are also added into query

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ const BookType = new GraphQLObjectType({
       type: AuthorType,
       resolve:(book)=>{
         return authors.find(author=>author.id == book.authorId)
-      }
+      } 
     }
   })
 })
@@ -51,7 +51,13 @@ const AuthorType = new GraphQLObjectType({
   description:"this is a single author of a book",
   fields:()=>({
     id: { type: GraphQLNonNull(GraphQLInt)},
-    name:{ type: GraphQLNonNull(GraphQLString)}
+    name:{ type: GraphQLNonNull(GraphQLString)},
+    books:{ 
+      type: new GraphQLList(BookType),
+      resolve:(author)=>{
+        return books.filter(book => book.authorId == author.id)
+      }
+    }
   })
 })
 
@@ -59,11 +65,33 @@ const RootQueryType = new GraphQLObjectType({
   name:"query",
   description:"Root Query",
   fields:()=>({
+     book:{
+      type: BookType,
+      description:"A single book",
+      args:{
+        id: { type: GraphQLInt }
+      },
+      resolve:(parent, args)=> books.find(book => book.id == args.id)
+     },
+     author:{
+      type: AuthorType,
+      description:"A single author",
+      args:{
+        id: { type: GraphQLInt }
+      },
+      resolve:(parent, args)=> authors.find(author => author.id == args.id)
+     },
      books:{
        type: new GraphQLList(BookType),
        description:"List of all books",
        resolve:()=>books
-      }
+      },
+     authors:{
+      type: new GraphQLList(AuthorType),
+      description:"List of all authors",
+      resolve:()=>authors
+
+     }
   })
 })
 
